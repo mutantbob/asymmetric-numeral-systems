5b7ee9d2eadc2eff1987c74dce616cfd482e32a1
Bob Forsman <bob.forsman@ericsson.com>
2021-04-09 11:20:32 -0400

    rename to ANSTableUniform; make logging controlled by a flag

--- short-ans/src/main.rs
+++ short-ans/src/main.rs
@@ -1,19 +1,21 @@
 extern crate symbol_table;
 
 use std::env;
+use std::fmt::{Display, LowerHex};
 use std::fs::File;
 use std::io::Error;
 use symbol_table::SymbolFrequencies;
 
-struct ANSTable {
+struct ANSTableUniform {
     frequencies: [u32; 256],
     sum_frequencies: u32,
     encode: Vec<Vec<u32>>,
     decode: Vec<(u8, u32)>,
+    verbose: bool,
 }
 
-impl ANSTable {
-    pub fn new(freqs: SymbolFrequencies) -> ANSTable {
+impl ANSTableUniform {
+    pub fn new(freqs: SymbolFrequencies) -> ANSTableUniform {
         let frequencies = freqs.frequencies;
         let sum_frequencies = frequencies.iter().sum();
         println!("sum_frequencies = {}", sum_frequencies);
@@ -39,18 +41,20 @@ impl ANSTable {
             }
         }
 
-        println!("{} better == {}", cursor, sum_frequencies);
+        assert!(cursor == sum_frequencies, "malfunction building symbol table: (cursor ={}) != (sum_frequencies={})", cursor, sum_frequencies);
+
         for (i, &a) in accum.iter().enumerate() {
             if a != 0 {
                 println!("unexpected accum[{}] == {}", i, a);
             }
         }
 
-        ANSTable {
+        ANSTableUniform {
             frequencies,
             sum_frequencies,
             encode: transforms,
             decode: backward,
+            verbose: false,
         }
     }
 
@@ -64,13 +68,33 @@ impl ANSTable {
         let phase = val % freq;
         let encoded = self.encode[symbol as usize][phase as usize];
         let rval = cycle * self.sum_frequencies + encoded;
-        if false {
-            println!(
-                "{} = {}*{} + {} ;  rval = {}*{} + {} = 0x{:x}",
-                val, freq, cycle, phase, self.sum_frequencies, cycle, encoded, rval
+        if self.verbose {
+            ANSTableUniform::log_encode(
+                val,
+                freq,
+                cycle,
+                phase,
+                encoded,
+                rval,
+                self.sum_frequencies,
             );
         }
         rval
+    }
+
+    fn log_encode<T: Display + LowerHex>(
+        val: T,
+        symbol_frequency: u32,
+        cycle: T,
+        phase: T,
+        encoded: u32,
+        rval: T,
+        sum_frequencies: u32,
+    ) {
+        println!(
+            "{} = {}*{} + {} ;  rval = {}*{} + {} = 0x{:x}",
+            val, symbol_frequency, cycle, phase, sum_frequencies, cycle, encoded, rval
+        );
     }
 
     pub fn append_encode64(&self, val: u64, symbol: u8) -> u64 {
@@ -79,10 +103,15 @@ impl ANSTable {
         let phase = val % (freq as u64);
         let encoded = self.encode[symbol as usize][phase as usize];
         let rval = cycle * (self.sum_frequencies as u64) + (encoded as u64);
-        if false {
-            println!(
-                "{} = {}*{} + {} ;  rval = {}*{} + {} = 0x{:x}",
-                val, freq, cycle, phase, self.sum_frequencies, cycle, encoded, rval
+        if self.verbose {
+            ANSTableUniform::log_encode(
+                val,
+                freq,
+                cycle,
+                phase,
+                encoded,
+                rval,
+                self.sum_frequencies,
             );
         }
         rval
@@ -93,22 +122,22 @@ impl ANSTable {
         let phase = val % self.sum_frequencies;
 
         let (symbol, tmp) = self.decode[phase as usize];
-        let rval = cycle * self.frequencies[symbol as usize] + tmp;
-        if false {
-            println!(
-                "{} = {}*{} + {}; rval = {}*{} + {} = 0x{:x}",
+        let sym_freq = self.frequencies[symbol as usize];
+        let rval = cycle * sym_freq + tmp;
+        if self.verbose {
+            ANSTableUniform::log_decode(
                 val,
                 self.sum_frequencies,
                 cycle,
                 phase,
-                rval,
-                self.frequencies[symbol as usize],
                 tmp,
-                rval
+                sym_freq,
+                rval,
             );
         }
         (symbol, rval)
     }
+
     pub fn decode64(&self, val: u64) -> (u8, u64) {
         let sum_frequencies = self.sum_frequencies as u64;
         let cycle = val / sum_frequencies;
@@ -117,13 +146,25 @@ impl ANSTable {
         let (symbol, tmp) = self.decode[phase as usize];
         let sym_freq = self.frequencies[symbol as usize];
         let rval = cycle * (sym_freq as u64) + (tmp as u64);
-        if false {
-            println!(
-                "{} = {}*{} + {}; rval = {}*{} + {} = 0x{:x}",
-                val, sum_frequencies, cycle, phase, rval, sym_freq, tmp, rval
-            );
+        if self.verbose {
+            ANSTableUniform::log_decode(val, sum_frequencies, cycle, phase, tmp, sym_freq, rval);
         }
         (symbol, rval)
+    }
+
+    fn log_decode<T: Display + LowerHex>(
+        val: T,
+        sum_frequencies: T,
+        cycle: T,
+        phase: T,
+        tmp: u32,
+        sym_freq: u32,
+        rval: T,
+    ) {
+        println!(
+            "{} = {}*{} + {}; rval = {}*{} + {} = 0x{:x}",
+            val, sum_frequencies, cycle, phase, rval, sym_freq, tmp, rval
+        );
     }
 }
 
@@ -158,7 +199,8 @@ fn demonstration1(fname: &str, test_data: &[u8]) -> Result<(), Error> {
     let mut symbol_file = File::open(fname)?;
     let frequencies = SymbolFrequencies::parse_binary_symbol_table(&mut symbol_file)?;
 
-    let ans_table = ANSTable::new(frequencies);
+    let mut ans_table = ANSTableUniform::new(frequencies);
+    //ans_table.verbose = true;
 
     let x = simple_encode(&ans_table, test_data);
 
@@ -176,7 +218,7 @@ fn demonstration1(fname: &str, test_data: &[u8]) -> Result<(), Error> {
     Ok(())
 }
 
-fn simple_encode(ans_table: &ANSTable, symbols: &[u8]) -> u64 {
+fn simple_encode(ans_table: &ANSTableUniform, symbols: &[u8]) -> u64 {
     let mut x = 0;
     for &symbol in symbols {
         x = ans_table.append_encode64(x, symbol);
@@ -184,7 +226,7 @@ fn simple_encode(ans_table: &ANSTable, symbols: &[u8]) -> u64 {
     x
 }
 
-fn simple_decode(ans_table: &ANSTable, mut val: u64) -> Vec<u8> {
+fn simple_decode(ans_table: &ANSTableUniform, mut val: u64) -> Vec<u8> {
     let mut rval = Vec::new();
     while val > 0 {
         let (symbol, new_val) = ans_table.decode64(val);
